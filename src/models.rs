d91bbbbe0c839bafac6a5980b00125a3f9e22686
Brendon Otto <brendon@brendonotto.com>
2022-01-15 21:17:53 -0600

    Started building out the POST endpoint, sqlx and Postgres need to agree on types though

--- src/models.rs
+++ src/models.rs
@@ -2,11 +2,13 @@ use actix_web::{Error, HttpRequest, HttpResponse, Responder};
 use anyhow::Result;
 use chrono::{DateTime, Local};
 use serde::{Serialize, Deserialize};
-use sqlx::{FromRow, Row, PgPool};
+use sqlx::postgres::PgRow;
+use sqlx::{FromRow, Row, PgPool, Executor};
 
 // Db model of temerature reading
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize, FromRow)]
 pub struct Reading {
+    pub id : i32,
     pub room : String,
     pub temperature : f64,
     pub humidity: f64,
@@ -42,5 +44,52 @@ impl Room {
             .collect();
 
         Ok(rooms)
+    }
+}
+
+impl Reading {
+    pub async fn create(reading: ReadingRequest, pool: &PgPool) -> Result<Reading> {
+
+        let mut tx = pool.begin().await?;
+
+        sqlx::query!(
+            r#"
+            SELECT r.id, $2, $3, current_timestamp
+            INTO TABLE temperature              
+            FROM room r WHERE room_name = $1
+            "#,
+            reading.room,
+            reading.temperature,
+            reading.humidity
+        )
+        .execute(&mut tx)
+        .await?;
+
+        let row_id : i32 = sqlx::query("SELECT last_insert_rowid()")
+            .map(|row: PgRow| row.get(0))
+            .fetch_one(&mut tx)
+            .await?;
+
+        let rec = sqlx::query!(
+            r#"
+            SELECT t.id, r.room_name, temperature, humidity, time 
+            FROM temperature t
+            INNER JOIN room r ON r.id = t.room_id
+            WHERE t.id = $1
+            "#,
+            row_id,
+        )
+        .fetch_one(&mut tx)
+        .await?;
+
+        tx.commit().await?;
+
+        Ok(Reading {
+            id: rec.id,
+            room: rec.room,
+            temperature: rec.temperature,
+            humidity: rec.humidity,
+            timestamp: rec.time
+        })
     }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, post, web, HttpResponse, Responder };
 use sqlx::PgPool;
 
-use crate::models::Room;
+use crate::models::{ Room, Reading, ReadingRequest };
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(rooms);
@@ -15,6 +15,17 @@ async fn rooms(db_pool: web::Data<PgPool>) -> impl Responder {
         Err(err) => {
             HttpResponse::InternalServerError()
                 .body("Error trying to read all rooms from database")
+        }
+    }
+}
+
+#[post("/reading")]
+async fn create(reading: web::Json<ReadingRequest>, db_pool: web::Data<PgPool>) -> impl Responder {
+    let result = Reading::create(reading.into_inner(), db_pool.get_ref()).await;
+    match result {
+        Ok(reading) => HttpResponse::Ok().json(reading),
+        Err(err) => {
+            HttpResponse::InternalServerError().body("Error creating new reading entry")
         }
     }
 }
